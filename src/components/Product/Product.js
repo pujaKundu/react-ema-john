b60bf2a397ab29eb7ae967f86373fdb099805904
Puja Kundu <pujakundu797@gmail.com>
2021-09-24 16:34:16 +0600

    cart added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product)
+    // console.log(props)
     const {name,img,seller,price,stock} = props.product;
     return (
         <div className="product">
@@ -12,10 +12,13 @@ const Product = (props) => {
             <div className="product-info">
                 <h4 className="product-name">{name}</h4>
                 <p><small>by: {seller}</small></p>
-                <p>Price: {price}</p>
+                <p>Price: ${price}</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
+                <button
+                onClick={()=>props.handleAddToCart(props.product)}
+                className="purchase-btn">add to cart</button>
             </div>
-            
+       
         </div>
     );
 };
